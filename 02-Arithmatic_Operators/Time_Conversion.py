d94c7b2c323a008cee8620fb576cd815565cce78
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2024-12-31 07:48:34 +0530

    Add files via upload

---
+++ 02-Arithmatic_Operators/Time_Conversion.py
@@ -0,0 +1,7 @@
+total_min = 125
+# hours = total_min / 60 
+hours = total_min // 60
+mins = total_min % 60
+print("hours",hours)
+print("minutes",mins)
+print(f"{hours} hours and {mins} minutes")  #formated print statement
---
+++ 02-Arithmatic_Operators/distributing_items_Evenly.py
@@ -0,0 +1,4 @@
+candies = 17
+children = 3
+candies_per_child = candies // children
+print(f"{candies_per_child} candies per child")  #output 5 (each child gets 5 candies)
