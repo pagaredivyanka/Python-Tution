ba8ffe0a9de8336dcb9fc931a399b8901fc6eb20
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2025-01-04 08:05:06 +0530

    user input

---
+++ 04-User_Input/userInput.py
@@ -0,0 +1,32 @@
+#String
+# name = input("Enter the nam : ")
+# print(name)
+
+# Full_Name = input("give your full name : ")
+# print(Full_Name)
+
+# #Integer
+
+# age = int(input("Enter your age : "))
+# print(age)
+
+#character
+
+# char = input("Enter character : ")
+# print(char)
+
+blood_group = input("Enter your blood group : ")
+print(blood_group)
+
+# assignment -> tip ->> can use formated string for better understanding of output
+
+#create registration form 
+#first name 
+#last name
+#email address
+#password
+#phone number
+#address -> street,city,state,country
+#college name -> name, address
+#marks 
+
