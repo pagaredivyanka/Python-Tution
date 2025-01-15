cb2108ee18d14f18b615df32d4c1a93ccd5bfb10
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2025-01-15 07:56:29 +0530

    Add files via upload

---
+++ 06-Bitwise_Operator/bitwise_not.py
@@ -0,0 +1,18 @@
+# bitwise not operator gives use opposite bits of the perticular number 
+#if 0 then -> 1
+#if 1 then -> 0
+
+# ex -> 
+
+# print(~5)
+
+# 101 -> -010
+
+a = int(input("Enter value of a: "))
+print(~a)
+
+# 0 -> -1
+# -1 => 0
+
+
+#          -7 -6 -5 -4 -3 -2 -1 | 0 1 2 3 4 5 6  
---
+++ 06-Bitwise_Operator/bitwise_xor.py
@@ -0,0 +1,20 @@
+# 5 XOR 4
+
+# 101
+# 100
+# ____
+# 001
+
+print(5 ^ 4)
+
+print(5 ^ 3)
+# 101
+# 011
+#-------
+# 110
+
+
+a = int(input("Enter number a : "))
+b = int(input("Enter number b : "))
+print("XOR of a and b = ", a ^ b)
+
