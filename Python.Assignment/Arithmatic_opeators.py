488f867e4a43a77163c1134eaed240342ec01f64
Poonam2005-ai <nandiwalep@gmail.com>
2025-01-15 20:54:16 -0500

    Assignment solutions

---
+++ Python.Assignment/Arithmatic_opeators.py
@@ -0,0 +1,14 @@
+Budget = 500
+
+breakfast = 100
+lunch = 100
+dinner = 100
+snacks = 100 
+
+total_expenses = breakfast + lunch + dinner + snacks 
+
+remaining_amount = Budget - total_expenses 
+print(f"total daily expenses :{total_expenses}") 
+if remaining_amount >=0:
+         print(f"amount left from budget:{remaining_amount}")
+   
---
+++ Python.Assignment/Budget_planner.py
@@ -0,0 +1,17 @@
+Income =30,000
+Expenses =25,000
+output_Remaining =5,000
+
+rent = 10000
+groceries = 7000
+utilities = 3000
+other_expenses = 5000
+
+total_expenses = rent + groceries + utilities + other_expenses
+
+remaining_amount = Income - total_expenses
+
+if remaining_amount >= 0:
+    print(f"Remaining Balance: {remaining_amount:,.2f}")
+else:
+    print(f"Deficit:  bs(remaining_amount):,.2f}")
