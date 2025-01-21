bdc8ed0cae14b222e441c4c1e8ce5c215e875210
Divyanka Pagare <113618065+pagaredivyanka@users.noreply.github.com>
2025-01-21 07:53:13 +0530

    Add files via upload

---
+++ 10-Type_Casting/00-typeCastingTypes.py
@@ -0,0 +1,35 @@
+# 1. int()
+a = int(34.87)  # float to integer
+print(a)  # 34
+print(type(a))  # <class 'int'>
+
+b = int("34")   # string to integer
+print(b)  # 34
+print(type(b))  # <class 'int'>
+
+# float()
+
+a = float(34)  # integer to float
+print(a)  # 34.0
+print(type(a))  # <class 'float'>
+
+
+c = "34.87"
+print(c) # 34.87
+print(type(c)) # <class 'str'>
+b = float("34.87")  # string to float
+b = float(c)  # string to float
+print(b)  # 34.87
+print(type(b))  # <class 'float'>
+
+# string
+
+a = 34
+b = str(a)
+print(a)  # 34
+print(type(a))  # <class 'int'>
+print(b)  # 34
+print(type(b))  # <class 'str'>
+
+#integer = 34
+#string = "34"
